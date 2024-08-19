e8eaab4865c86de7ba385f8fda19b76e18bf8aa0
Anvita Rother Pal <anvita.r.palchitkara@gmail.com>
2024-08-19 15:22:58 -0400

    Added NoArgsConstructor notation to JournalEntry

--- src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
+++ src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
@@ -8,7 +8,8 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.Date;
 
 @Document(collection="journal_entries")
-
+@Data
+@NoArgsConstructor
 public class JournalEntry {
 
     @Id
