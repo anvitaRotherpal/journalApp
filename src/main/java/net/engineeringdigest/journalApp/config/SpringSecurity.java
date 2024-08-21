e48717fd7dd07cdef8718c58139ae1be9e2d2437
Anvita Rother Pal <anvita.r.palchitkara@gmail.com>
2024-08-21 14:54:14 -0400

    Added security Configuration to configure method

--- src/main/java/net/engineeringdigest/journalApp/config/SpringSecurity.java
+++ src/main/java/net/engineeringdigest/journalApp/config/SpringSecurity.java
@@ -4,5 +4,15 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableWebSecurity
-public class SpringSecurity {
+public class SpringSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity) throws Exception{
+        http.authorizeRequests()
+                .antMatchers("/journal/**").authenticated()
+                .anyRequest.permitAll()
+                .and()
+                .HttpBasic()
+
+    }
 }
