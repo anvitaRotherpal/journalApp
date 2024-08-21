195fc852d0fef925930e66ec74014ea54b868839
Anvita Rother Pal <anvita.r.palchitkara@gmail.com>
2024-08-21 13:51:17 -0400

    Created a new class called SpringSecurity in config package with the necessary aannotations

---
+++ src/main/java/net/engineeringdigest/journalApp/config/SpringSecurity.java
@@ -0,0 +1,8 @@
+package net.engineeringdigest.journalApp.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurity {
+}
