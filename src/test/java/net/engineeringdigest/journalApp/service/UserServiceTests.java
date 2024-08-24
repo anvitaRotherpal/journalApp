510b635566414378735d69c3537b1767ae1f3e7b
Anvita Rother Pal <anvita.r.palchitkara@gmail.com>
2024-08-24 18:38:17 -0400

    added parametrized test

--- src/test/java/net/engineeringdigest/journalApp/service/UserServiceTests.java
+++ src/test/java/net/engineeringdigest/journalApp/service/UserServiceTests.java
@@ -1,4 +1,32 @@
 package net.engineeringdigest.journalApp.service;
 
+import net.engineeringdigest.journalApp.Repositories.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class UserServiceTests {
+
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void testFindByUserName() {
+        userRepository.findByUserName("ram");
+
+
+    }
+
+
+    @ParametrizedTest
+    @CsvSource({
+            "1,1,2",
+            "2,10,12"
+    })
+    public void test(int a, int b, int expected){
+        assertEquals(expected, a+b);
+    }
 }
