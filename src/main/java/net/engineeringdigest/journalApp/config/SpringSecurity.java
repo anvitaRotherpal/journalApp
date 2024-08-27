8107cbfdb0746718e83729525522cf169893e9d8
Anvita Rother Pal <anvita.r.palchitkara@gmail.com>
2024-08-27 12:28:00 -0400

    Added dev profile as an annotation

--- src/main/java/net/engineeringdigest/journalApp/config/SpringSecurity.java
+++ src/main/java/net/engineeringdigest/journalApp/config/SpringSecurity.java
@@ -2,9 +2,11 @@ package net.engineeringdigest.journalApp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 @EnableWebSecurity
+@Profile("dev")
 public class SpringSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
