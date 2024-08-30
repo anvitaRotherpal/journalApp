fdd40f31a1b9986149f6920077a3cc6e31254644
Anvita Rother Pal <anvita.r.palchitkara@gmail.com>
2024-08-30 14:53:50 -0400

    Added getWeather method and other annotations to get the required API

---
+++ src/main/java/net/engineeringdigest/journalApp/service/weatherService.java
@@ -0,0 +1,21 @@
+package net.engineeringdigest.journalApp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+public class weatherService {
+
+    @Component
+    private static final String apiKey="2c774f65e4d764b3b9e1d359246edbc3";
+
+    private static final String API="https://api.weatherstack.com/current?access_key=API_key&query=CITY";
+
+     @Autowired
+    private RestTemplate restTemplate;
+
+
+     public String getWeather(String city){
+         String finalAPI=API.replace("CITY",city).replace("API_KEY",apiKey);
+     }
+}
