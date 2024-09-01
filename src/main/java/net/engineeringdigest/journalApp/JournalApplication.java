7bca02470e7840bfa721be3833b9deb5ad76fd4b
Anvita Rother Pal <anvita.r.palchitkara@gmail.com>
2024-09-01 11:53:43 -0400

    Added restTemplate function

--- src/main/java/net/engineeringdigest/journalApp/JournalApplication.java
+++ src/main/java/net/engineeringdigest/journalApp/JournalApplication.java
@@ -4,7 +4,11 @@ package net.engineeringdigest.journalApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableTransactionManagement
@@ -14,14 +18,15 @@ public class JournalApplication {
         SpringApplication.run(JournalApplication.class, args);
     }
 
-    public void main() {
 
-
+    @Bean
+    public PlatformTransactionManager abc(MongoDatabaseFactory dbFactory){
+        return new MongoTransactionManager(dbFactory);
     }
 
     @Bean
-    public int add(int i){
-        return i;
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 
 
