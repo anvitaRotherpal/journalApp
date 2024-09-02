c3da78dcbb9a8e9c61216ed7a5ca7915e8700f25
Anvita Rother Pal <anvita.r.palchitkara@gmail.com>
2024-09-02 14:33:24 -0400

    Created a list under PostConstruct annotation

--- src/main/java/net/engineeringdigest/journalApp/cache/AppCache.java
+++ src/main/java/net/engineeringdigest/journalApp/cache/AppCache.java
@@ -1,9 +1,11 @@
 package net.engineeringdigest.journalApp.cache;
 
 import net.engineeringdigest.journalApp.Repositories.ConfigJournalAppRepository;
+import net.engineeringdigest.journalApp.entity.ConfigJournalAppEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 import java.util.Map;
 
 public class AppCache  {
@@ -12,10 +14,13 @@ public class AppCache  {
     private ConfigJournalAppRepository configJournalAppRepository;
 
 
-    private Map<String, String>appCache;
+    private Map<String, String>APP_CACHE;
 
     @PostConstruct
     public void init() {
-        appCache=null;
+        List<ConfigJournalAppEntity> all=configJournalAppRepository.findAll();
+        for(ConfigJournalAppEntity configJournalAppEntity:all) {
+            APP_CACHE.put(configJournalAppEntity.getKey(), configJournalAppEntity.getValue());
+        }
     }
 }
