ed463bb608959f47576a022b609957214824ff7b
Anvita Rother Pal <anvita.r.palchitkara@gmail.com>
2024-09-03 14:43:26 -0400

    Added sendEmail method

--- src/main/java/net/engineeringdigest/journalApp/service/EmailService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/EmailService.java
@@ -1,6 +1,24 @@
 package net.engineeringdigest.journalApp.service;
 
+import ch.qos.logback.classic.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 public class EmailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+
+    public void sendEmail(String to, String subject, String body) {
+        try{
+            SimpleMailMessage mail=new SimpleMailMessage();
+            mail.setTo(to);
+            mail.setSubject(subject);
+            mail.setText(body);
+            javaMailSender.send(mail);
+        }catch(Exception e){
+            log.error("Exception while sendEmail", e);
+        }
+    }
 }
