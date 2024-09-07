e5446a1dcfb3d0ecfef1cf1bc903d94d995867ba
Anvita Rother Pal <anvita.r.palchitkara@gmail.com>
2024-09-07 18:09:16 -0400

    Added scheduled annotation and cron to facilitate timely transfer of email every Sunday

--- src/main/java/net/engineeringdigest/journalApp/Scheduler/UserScheduler.java
+++ src/main/java/net/engineeringdigest/journalApp/Scheduler/UserScheduler.java
@@ -1,8 +1,14 @@
 package net.engineeringdigest.journalApp.Scheduler;
 import net.engineeringdigest.journalApp.Repositories.UserRepository;
+import net.engineeringdigest.journalApp.entity.JournalEntry;
+import net.engineeringdigest.journalApp.entity.User;
 import net.engineeringdigest.journalApp.service.EmailService;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Component
@@ -14,9 +20,17 @@ public class UserScheduler {
     @Autowired
     private UserRepository userRepository;
 
+    @Scheduled(cron="0 9 * * SUN")
     public void fetchUserAndSendMail(){
 
-        @Autowired
-        EmailService emailService ;
+        List<User> users=userRepository.getUserforSA();
+        for(User user:users){
+            List<JournalEntry> journalEntries=user.getJournalEntries();
+            List<JournalEntry> filteredEntries=journalEntries.stream().filter(x -> x.getDate().isAfter(LocalDateTime.now().minus(7, ChronoUnit.DAYS))).map(x->x.getContent().collect(Collectors.toList()));
+            String entry=String.join("", filteredEntries);
+            int sentiment=sentimentAnalysisService.getSentiment(entry);
+            emailService.sendEmail(user.getEmail(), "Sentiment for last 7 days",sentiment);
+        }
+
     }
 }
