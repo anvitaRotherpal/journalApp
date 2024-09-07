7325c0303e57386e7420afce52e6010951b62a2e
Anvita Rother Pal <anvita.r.palchitkara@gmail.com>
2024-09-07 17:49:43 -0400

    Added getSentiment function

--- src/main/java/net/engineeringdigest/journalApp/service/SentimentAnalysisService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/SentimentAnalysisService.java
@@ -3,6 +3,10 @@ package net.engineeringdigest.journalApp.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class SentimentAnalysis {
+public class SentimentAnalysisService {
+
+    public int getSentiment(String text) {
+        return 1;
+    }
 
 }
