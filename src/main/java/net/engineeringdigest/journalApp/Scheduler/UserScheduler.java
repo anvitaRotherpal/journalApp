8ea030ba873182a99402e078edb55335843370f1
Anvita Rother Pal <anvita.r.palchitkara@gmail.com>
2024-09-07 17:55:10 -0400

    Emailed UserService and UserRepositoryImpl

--- src/main/java/net/engineeringdigest/journalApp/Scheduler/UserScheduler.java
+++ src/main/java/net/engineeringdigest/journalApp/Scheduler/UserScheduler.java
@@ -1,6 +1,22 @@
 package net.engineeringdigest.journalApp.Scheduler;
+import net.engineeringdigest.journalApp.Repositories.UserRepository;
+import net.engineeringdigest.journalApp.service.EmailService;
 import org.springframework.stereotype.Component;
+
+
 
 @Component
 public class UserScheduler {
+
+    @Autowired
+    private EmailService emailService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void fetchUserAndSendMail(){
+
+        @Autowired
+        EmailService emailService ;
+    }
 }
